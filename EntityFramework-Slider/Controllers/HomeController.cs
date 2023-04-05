ba812333101c3c913b17e6bde21ded55687b5f84
Faridrab <faridrab@code.edu.az>
2023-04-06 01:17:02 +0400

    framework

---
+++ EntityFramework-Slider/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using EntityFramework_Slider.Data;
+using EntityFramework_Slider.Models;
+using EntityFramework_Slider.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace EntityFramework_Slider.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> sliders = await _context.Sliders.ToListAsync();
+
+            SliderInfo sliderInfo = await _context.SliderInfos.FirstOrDefaultAsync();
+
+            IEnumerable<Blog> blogs = await _context.Blogs.Where(m=>!m.SoftDelete).ToListAsync();
+
+            IEnumerable<Category> categories = await _context.Categories.Where(m => !m.SoftDelete).ToListAsync();
+
+            IEnumerable<Product> products = await _context.Products.Include(m=>m.Images).Where(m => !m.SoftDelete).ToListAsync();
+
+
+            HomeVM model = new()
+            {
+                Sliders = sliders,
+                SliderInfo = sliderInfo,
+                Blogs = blogs,
+                Categories = categories,
+                Products = products
+            };
+
+            return View(model);
+        }
+    }
+}
